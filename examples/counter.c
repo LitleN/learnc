9ada8683bd47e6c0e7c1918f06ca3737e2d36346
Nadya <villiwonka@gmail.com>
2019-07-20 20:56:22 +0300

    Getchar

---
+++ examples/counter.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int str, space, tab, finish;
+    finish = 0;
+    space = 0;
+    tab = 0;
+
+    printf("Введите текст, в котором хотите посчитать пробелы, знаки табуляции и символы конца строки. По окончании ввода нажмите +\n");
+    while ((str = getchar()) != EOF){
+        if(str == ' '){
+            space++;
+        } 
+        else if(str == '\t'){
+            tab++;
+        }
+        else if(str == '\n'){
+            finish++;
+        }
+        else if(str == '+'){
+            break;
+        }
+    }
+    printf("В строке %i пробелов, %i знаков табуляции, %i символов конца строки\n", space, tab, finish);
+    return 0;
+}
